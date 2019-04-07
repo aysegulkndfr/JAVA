542966b35816034ce42185db2a7179457ff049a9
meysaru <47474785+meysaru@users.noreply.github.com>
2019-04-07 15:49:19 +0300

    Create tekCift.java

---
+++ tekCift.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class tekCift{
+    public static void main(String[] args){
+        int sayi = 0;
+        Scanner gir = new Scanner(System.in);
+        System.out.println("sayi gir: ");
+        sayi = gir.nextInt();
+        if(sayi %2 == 0){
+            System.out.println("cift sayi");
+        }
+        else{
+            System.out.println("tek sayi");
+        }
+    }
+}
